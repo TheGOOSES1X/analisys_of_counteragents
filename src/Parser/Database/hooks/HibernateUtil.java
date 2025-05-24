7bf8e8494b4c7164a6095810c8981b3458d0507c
Sergey Yakupov <motherboard98@yandex.ru>
2025-05-24 21:32:41 +0300

    Подключение к бд

--- src/Parser/Database/hooks/HibernateUtil.java
+++ src/Parser/Database/hooks/HibernateUtil.java
@@ -50,12 +50,12 @@ public class HibernateUtil {
 
                 Map<String, Object> settings = new HashMap<>();
                 settings.put(Environment.DRIVER, "org.postgresql.Driver");
-                settings.put(Environment.URL, String.format("jdbc:postgresql://%s:%s/%s",
-                        config.optString("HOST", "192.168.234.237"),
-                        config.optString("PORT", "5432"),
-                        config.getString("DB_Global_Module")));
-                settings.put(Environment.USER, config.getString("USER"));
-                settings.put(Environment.PASS, config.getString("PASSWORD"));
+                settings.put(Environment.URL, "jdbc:postgresql://" +
+                        config.optString("HOST", "192.168.234.237") + ":" +
+                        config.optString("PORT", "5432") + "/" +
+                        config.getString("DB_Global_Module")); // global_module_238
+                settings.put(Environment.USER, config.getString("USER")); // postgres
+                settings.put(Environment.PASS, config.getString("PASSWORD")); // globalA17P14
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 // Только при первом запуске - создаем схему
