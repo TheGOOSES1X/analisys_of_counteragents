342009a992ac0a3422b2968c90a9a20f9c7cefc4
Kirill Andreev <61062190+TheGOOSES1X@users.noreply.github.com>
2025-07-09 16:02:00 +0300

    ИТОГ для завода, конфиг стоит заводской

--- src/Parser_EGRUL/ConnectToDB_Start.java
+++ src/Parser_EGRUL/ConnectToDB_Start.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public class ConnectToDB_Start {
     public static List<String> CollectINNFromDB() {
-        String url = "jdbc:postgresql://192.168.234.237:5432/global_module_238";
+        String url = "jdbc:postgresql://10.210.12.212:5432/global_module";
         String user = "postgres";
-        String password = "globalA17P14";
+        String password = "postgres";
 
         List<String> INN_list = new ArrayList<>();
 
